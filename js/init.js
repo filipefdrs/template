fa7411345208525fb448ece7a354c95775f88ba8
Filipe Reis <38828226+filipedosreissantos@users.noreply.github.com>
2020-12-22 23:55:12 -0300

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,13 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.dropdown-trigger').dropdown({hover: false});
+    $('.carousel.carousel-slider').carousel({fullWidth: true, indicators: true});
+    $('.modal').modal();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space 
+
+
+        
