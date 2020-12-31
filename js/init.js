19f690a629978ee601fdb78ad4bd9a05d869eac6
Filipe Reis <38828226+filipefdrs@users.noreply.github.com>
2020-12-31 19:14:56 -0300

    Add files via upload

--- js/init.js
+++ js/init.js
@@ -4,6 +4,8 @@
     $('.sidenav').sidenav();
     $('.dropdown-trigger').dropdown({hover: false});
     $('.carousel.carousel-slider').carousel({fullWidth: true, indicators: true});
+    setInterval(function() {$('.carousel').carousel('next');}, 3000);
+    $('.collapsible').collapsible();
     $('.modal').modal();
 
   }); // end of document ready
